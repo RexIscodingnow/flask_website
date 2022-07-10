82db7c17eea4a8c2e7d1f9a3ede0d8a929733da6
Huang yao ting <a0916996002@gmail.com>
2022-07-11 00:09:50 +0800

    CommitInfo

--- SQLite.py
+++ SQLite.py
@@ -1,40 +1,6 @@
 from app import db
 
 '''
-    普通 sqlite 的建立的方式
-
-    說明 => __tablename__ = '資料庫的 table 名稱' : 若沒有設置，預設使用 class 的名稱
-            primary_key = True : 基本上要有主鍵
-
-            以下是我用過 der...
-
-            db.欄位功能                   | Python
-       |----------------------------------|------------------|
-       | 1. Column() : 配置欄位            |                  |
-       | 2. Integer : 32 bit 的整數        | int              |
-       | 3. SmallInteger : 16 bit 的整數   | int              |
-       | 4. BigInteger : 不限精度 的整數   | int              |
-       | 5. Float : 浮點數                 | float            |
-       |-----------------------------------|------------------|
-       | 6. String(字數) : 文字            | str              |
-       | 7. Text() : 長字串                | str              |
-       | 8. LargeBinary : 二進制文件       | str              |
-       | 9. PickleType : 序列化文件        | object           |
-       |-----------------------------------------------------|
-'''
-class User(db.Model):
-    __tablename__ = 'users'     # sqlite 資料庫的 tablename
-    id = db.Column(db.Integer, primary_key=True)    # 設定主鍵
-    # String -> 文字型態  unique -> 唯一值  nullable -> True: 可空格, False: 不能為空
-    # String(50) -> 最多50字
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-
-    def __repr__(self):
-        return "<User %r>" % self.username
-
-'''
     權限篇，日後更新
 '''
 ##  設置中繼的關聯表
--- app.py
+++ app.py
@@ -8,22 +8,53 @@ from flask_sqlalchemy import SQLAlchemy
 # from pymongo import MongoClient
 from flask_bcrypt import Bcrypt
 from form import RegisterForm, LoginForm
-import os
-
-#  取得目前文件資料夾路徑
-pjdir = os.path.abspath(os.path.dirname(__file__))
+# import os
 
 app = Flask(__name__) # __name__代表目前執行的模組
 
 #  新版本的部份預設為none，會有異常，再設置True即可。
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 # #  設置sqlite檔案路徑
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-    os.path.join(pjdir, 'data.sqlite')
-app.config['SECRET_KEY'] = 'fjfisljdqoiahf;laojdqahlwdjayfghlkjd'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+#     os.path.join(pjdir, 'data.sqlite')
+# app.config['SECRET_KEY'] = 'fjfisljdqoiahf;laojdqahlwdjayfghlkjd'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
+'''
+    普通 sqlite 的建立的方式
+
+    說明 => __tablename__ = '資料庫的 table 名稱' : 若沒有設置，預設使用 class 的名稱
+            primary_key = True : 基本上要有主鍵
+
+            以下是我用過 der...
+
+            db.欄位功能                   | Python
+       |----------------------------------|------------------|
+       | 1. Column() : 配置欄位            |                  |
+       | 2. Integer : 32 bit 的整數        | int              |
+       | 3. SmallInteger : 16 bit 的整數   | int              |
+       | 4. BigInteger : 不限精度 的整數   | int              |
+       | 5. Float : 浮點數                 | float            |
+       |-----------------------------------|------------------|
+       | 6. String(字數) : 文字            | str              |
+       | 7. Text() : 長字串                | str              |
+       | 8. LargeBinary : 二進制文件       | str              |
+       | 9. PickleType : 序列化文件        | object           |
+       |-----------------------------------------------------|
+'''
+class User(db.Model):
+    __tablename__ = 'users'     # sqlite 資料庫的 tablename
+    id = db.Column(db.Integer, primary_key=True)    # 設定主鍵
+    # String -> 文字型態  unique -> 唯一值  nullable -> True: 可空格, False: 不能為空
+    # String(50) -> 最多50字
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return "<User %r>" % self.username
 
 @app.route("/", methods=['GET', 'POST'])  # 函式的裝飾 (Decorator): 已函式為基礎，提供附加的功能
 def index():
