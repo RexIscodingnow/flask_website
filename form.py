03375aa460eca9687800d8ccf0e0be13eace2c96
Huang yao ting <a0916996002@gmail.com>
2022-11-22 09:10:14 +0800

    註解遷移與排版整理

--- form.py
+++ form.py
@@ -1,31 +1,5 @@
 '''
-    flask_wtf 表單設置
-
-    用 class 作為 "每一種頁面的表單"
     
-    (使用前，先繼承老爸的能力)
-    使用前先繼承 flask_wtf 模組，裡面的 class => FlaskForm
-
-    (防守狀態: 我方發動 XXXXX 驗證器，作為防守。 防守效果: 格式錯誤，不給通過，若對手攻擊大於 500 點，即刻反彈對手攻擊，再乘兩倍的 HP 傷害......喂!中二病發作喔!)
-    在 wtforms 底下的功能，其中有驗證器檢查輸入內容
-    比如說: 必填選項 => DataRequired()
-            相同內容 => EqualTo()  應用場景: 註冊帳號、修改密碼 的 再輸入一次密碼
-            
-            其他要輸入特定格式的欄位，檢查有沒有符合格式
-
-            ** 註: DataRequired() 與 EqualTo() ---->  在 wtforms.validators 底下
-
-    導入 wtforms 模組，它底下的欄位
-
-    欄位們: 
-            1. 輸入框 -----> 單行: StringField()
-                      -----> 多行: TextAreaField()
-            1-1. 密碼 -----> PasswordField()
-            1-2. 電子郵件 -----> EmailField()
-
-            3. 送出按鍵 -----> SubmitField()
-            ...
-            ...
 '''
 
 from flask_wtf import FlaskForm   # 要先繼承這個類別
