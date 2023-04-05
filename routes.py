779edcd8e1c53d367a4020a46ab8ad2fb5ff6945
Huang yao ting <a0916996002@gmail.com>
2023-04-05 10:09:11 +0800

    Add: route => goods(), forms

--- routes.py
+++ routes.py
@@ -3,15 +3,26 @@
 '''
 
 # Flask 的函式與功能
-from flask import session
-from flask_modules import app, render_template, redirect, url_for, request
+from flask_modules import app, render_template, redirect, url_for, request, session, Response
 # 身分驗證 的函式與功能
 from flask_modules import login_manager, login_user, login_required, current_user, logout_user, UserMixin
 
-from form import RegisterForm, LoginForm, ForgotPassword    # flask 表單 && wtform 模組
-from sqlite import db, Users    # 資料庫管理
+# 限速器
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
-from datetime import timedelta
+from form import RegisterForm, LoginForm, ForgotPassword, GoodsPageForm    # flask 表單 && wtform 模組
+from sqlite import db, Users, OtherData    # 資料庫管理
+
+from datetime import timedelta, datetime
+
+
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=['50 per hour', '10 per minute']
+)
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -36,6 +47,10 @@ def register():
     '''
         註冊功能，新增到資料庫
     '''
+    # 已經登入過，則重新導向到 首頁
+    if "user" in session:
+        return redirect(url_for("index"))
+
     # 表單項目。 用 render_template() 的參數 **context 丟給目標 .html 檔案，呈現表單項目
     form = RegisterForm()
     
@@ -64,6 +79,9 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if "user" in session:
+        return redirect(url_for("index"))
+
     form = LoginForm()
 
     if form.validate_on_submit():
@@ -71,15 +89,17 @@ def login():
         user = Users.query.filter_by(email=form.email.data).first()   # 以電子郵件欄位，作為搜尋條件
         if user:
             if user.check_password(form.password.data):
-                remember_time = timedelta(form.remember_time.data)
-                
+                # session.permanent = True
+                remember_time = timedelta(minutes=form.remember_time.data)
+                # session['user'] = form.email.data   # 新增一個 session
+
                 # login_user(當下登入的使用者, 記住登入(自訂時間，要設為 True), 記住登入時間長度)
                 login_user(user, remember=True, duration=remember_time)
                 next = request.args.get('next')
                 if not next_is_valid(next):
                     return "uhhhhhhhhhh.......uhhhhhhhhhhhhhhhhh......."
 
-                return redirect(url_for("user") or next)
+                return redirect(url_for("index") or next)
 
             else:
                 return render_template("login.html", form=form, msg="密碼輸入有誤!")
@@ -90,6 +110,7 @@ def login():
     return render_template("login.html", form=form)
 
 def next_is_valid(url):
+    # print(url)
     return True
 
 @app.route('/user')
@@ -99,17 +120,54 @@ def user():
         @login_required 裝飾器
         是用在需要登入，才能訪問的頁面
     '''
+    # print(request.cookies.get("remember_token"))
     return render_template("user.html")
+
+
+def manageGoods(number):
+    if number == 0:
+        form = GoodsPageForm()
+        messages = ["該車(牌)號(碼)", "該標題", " 已經紀錄了"]
+        msg = ""
+        if form.validate_on_submit():
+            n_number = form.n_number.data
+            n_title = form.n_title.data
+
+            exist_number = OtherData.query.filter_by(numbers = n_number).first()
+            exist_title = OtherData.query.filter_by(titles = n_title).first()
+            if exist_number:
+                msg = messages[0] + messages[2]
+            elif exist_title:
+                msg = messages[1] + messages[2]
+
+            else:
+                datas = OtherData()
+                datas.numbers = n_number
+                datas.titles = n_title
+
+                db.session.add(datas)
+                db.session.commit()
+
+        return render_template("goods_home.html", form=form, msg=msg)
+
+    elif number == 1:
+        return render_template("goods_addInfo.html")
+
+# 網址: /goods/1
+@app.route("/goods/<int:number>", methods=['GET', 'POST'])
+def goods(number=0):
+    return manageGoods(number)
+
 
 @app.route('/logout')
 @login_required
 def logout():
     '''
     登出功能 => logout_user()
-    清除伺服端的標識 id => session.pop() 
+    清除伺服端的標識 (cookie) => session.pop() 
     '''
     logout_user()
-    # session.pop()
+    session.pop("user", None)
     return redirect(url_for("login"))
 
 @app.route('/forgot_pw', methods=['GET', 'POST'])
@@ -121,16 +179,13 @@ def forgotPw():
     form = ForgotPassword()
 
     if form.validate_on_submit():
+        # 取的(查詢) 用戶的資訊
         user = Users.query.filter_by(email=form.email.data).first()
-        
         if user:
             password = form.new_password.data
+            user.password_hash = password   # 把用戶的密碼，做更改
             
-            user.password_hash = password
             db.session.add(user)
             db.session.commit()
 
     return render_template("forgot_password.html", form = form)
-
-if __name__ == "__main__": # 如果以主程式執行
-    app.run(debug=True)
